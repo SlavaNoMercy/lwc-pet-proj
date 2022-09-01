19767cd47b86c0bedb4105d95d8eb42edd833509
SlavaNoMercy <slava.nomercy@gmail.com>
2022-09-01 18:19:50 +0300

    carousel update

--- force-app/main/default/lwc/carouselView/carouselView.js
+++ force-app/main/default/lwc/carouselView/carouselView.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class CarouselView extends LightningElement {}
+export default class CarouselView extends LightningElement {
+    
+}
--- force-app/main/default/lwc/centralWindow/centralWindow.js
+++ force-app/main/default/lwc/centralWindow/centralWindow.js
@@ -1,3 +1,46 @@
 import { LightningElement } from 'lwc';
+import TEST_IMAGES from '@salesforce/resourceUrl/LadaCenterHome';
 
-export default class CentralWindow extends LightningElement {}
+export default class CentralWindow extends LightningElement {
+
+    testStruct = [
+        TEST_IMAGES + '/ladaHomePage.jpg',
+        TEST_IMAGES + '/ladaGranta.jpg',
+        TEST_IMAGES + '/ladaNiva.jpg',
+        TEST_IMAGES + '/ladaVesta.jpg',
+        TEST_IMAGES + '/ladaXRAY.jpg',
+        TEST_IMAGES + '/ladaNiva.jpg'
+    ]
+
+    currentIndex = 0;
+
+    get currentSlot(){
+        return this.testStruct[this.currentIndex];
+    }
+    
+    get prevSlot(){
+        if(this.currentIndex===0){
+            return this.testStruct[this.testStruct.length-1];
+        }
+        else{
+            return this.testStruct[this.currentIndex-1];
+        }
+    }
+
+    get nextSlot(){
+        if(this.currentIndex===this.testStruct.length-1){
+            return this.testStruct[0];
+        }
+        else{
+            return this.testStruct[this.currentIndex+1];
+        }
+    }
+
+    delayF(){
+        this.currentIndex += 1;
+        if(this.currentIndex===this.testStruct.length-1){
+            this.currentIndex = 0;
+        }
+        setTimeout(() => this.delayF(),8000);
+    }
+}
